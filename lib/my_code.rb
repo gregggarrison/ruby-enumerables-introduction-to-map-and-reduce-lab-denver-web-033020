bda24bdb47deff7e132efbaf2d73b8fa77808636
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-21 17:35:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,14 @@ def map_to_negativize(source_array)
   neg_arr
 end 
   
+def map_to_no_change(source_array)
+  counter = 0
+  new_arr = []
+  while counter < source_array.length
+  new_result = source_array[counter] * 1
+  new_arr << new_result
+  counter += 1
+  end  
+  new_arr
+end 
+map_to_no_change(arr)
