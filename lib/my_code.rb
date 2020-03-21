7c9b6ad5e2663ec9f6483e63d147e6d815c3c9bc
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-21 17:40:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,3 +19,14 @@ def map_to_no_change(source_array)
   end  
   new_arr
 end 
+
+def map_to_double(source_array)
+  counter = 0 
+  dbl_arr = []
+  while counter < source_array.length
+  dbl_result = source_array[counter] * 2
+  dbl_arr << dbl_result
+  counter += 1 
+  end
+  dbl_arr
+end 
