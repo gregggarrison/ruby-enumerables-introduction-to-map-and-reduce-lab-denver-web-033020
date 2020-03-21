eaf5786ede56da15c7d7cd7e71a9672f71cd867b
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-21 18:00:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,7 @@ def map_to_square(source_array)
   squ_arr
 end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   counter = 0
   total = 0
   starting_point = source_array.length - source_array.length 
